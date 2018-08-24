741e696fd68e44830ef0f345340c3465311aa21c
rajeshqsbio <32333617+rajeshbabua@users.noreply.github.com>
2018-08-24 14:27:40 +0530

    Update fg.py

--- fg.py
+++ fg.py
@@ -1,3 +1,3 @@
 import face_recognition
-image = face_recognition.load_image_file("your_file.jpg")
+image = face_recognition.load_image_file("scientist.jpeg")
 face_locations = face_recognition.face_locations(image)
